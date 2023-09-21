79976dd81506f88b02e39db7856a285607eb3c3b
Sebastian Kuś <sebastian.kus75@gmail.com>
2023-09-21 20:49:34 +0200

    Zadanie domowe Dzień 4

--- Challenge App/Challenge App/Program.cs
+++ Challenge App/Challenge App/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.ComponentModel.Design;
+
+string name = ("Ewa");
+string sex = ("Kobieta");
+var age = 29;
+if (sex == "Kobieta" && age < 30)
+{
+    Console.WriteLine("KObieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (name != "Ewa" && age <= 18)
+{
+    Console.WriteLine("nie pełnoletni mężczyzna");
+}
+else
+{
+    Console.WriteLine("pelnoletni mężczyzna");
+}
